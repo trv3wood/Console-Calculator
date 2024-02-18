d82c5844690e4281057f5ab64c2dc6aedbd58066
Z YS <1841369469@qq.com>
2024-02-18 20:35:00 +0800

    更新 calculate.cpp

--- src/console/calculate.cpp
+++ src/console/calculate.cpp
@@ -3,7 +3,7 @@
 
 std::string calculate(const Float& num1, const Float& num2, std::string operation) {
     Float res;
-    if (operation == "=") {
+    if (operation == "+") {
         res = num1 + num2;
     } else if (operation == "-") {
         res = num1 - num2;
